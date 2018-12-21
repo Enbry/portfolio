4e73b4b698895d3ef1bf59da5bee581479e2170f
Oonan <brocheray.noemie@gmail.com>
2018-12-21 16:21:10 +0100

    Update header

--- src/containers/Home/Home.js
+++ src/containers/Home/Home.js
@@ -1,5 +1,8 @@
 import React from "react";
 import "./Home.scss";
+import ReactSVG from "react-svg";
+import letterN from "../../images/n.svg";
+import letterB from "../../images/b.svg";
 
 class Home extends React.Component {
   constructor(props) {
@@ -7,7 +10,29 @@ class Home extends React.Component {
   }
 
   render() {
-    return <div className="home" />;
+    return (
+      <div>
+        <div className="home">
+          <div className="title">
+            <h1>
+              <ReactSVG
+                src={letterN}
+                className="wrapper-title"
+                svgClassName="letter-n"
+              />
+              <ReactSVG
+                src={letterB}
+                className="wrapper-title"
+                svgClassName="letter-b"
+              />
+            </h1>
+            <h2>Développeur front end</h2>
+          </div>
+          <div className="canvas" />
+        </div>
+        <div class="bomb" />
+      </div>
+    );
   }
 }
 
